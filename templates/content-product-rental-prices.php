7e5beb1894d6fd5f03ec00990130458edbb5c6cf
webexpert123 <42173715+webexpert123@users.noreply.github.com>
2019-05-03 19:24:42 +0530

    fixed for date selection for rental product.

--- templates/content-product-rental-prices.php
+++ templates/content-product-rental-prices.php
@@ -6,15 +6,17 @@
 
 //Exit on unecessary access
 defined('ABSPATH') or exit;
-if( is_null($first_item) ){
-    $first_item = false;
-}
+
+	if( is_null($first_item) ){
+	    $first_item = false;
+	}
 ?>
 
 <div class="wrp_content_product_rental_prices">
 
     <?php if( $first_item === false ): ?>
-    <h4>Choose Rental Options</h4>
+    <h4>Choose Rental Dates</h4>
+
     <?php endif; ?>
 
     <?php echo $this->format_rental_price_table($data, $first_item); ?>
